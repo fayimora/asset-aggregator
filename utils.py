2d378cb996244cd77be95815e71c9dec6873144f
Fayimora Femi-Balogun <fayi@fayimora.com>
2016-12-13 09:19:50 +0000

    Document utils

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ import codecs
 
 
 def requests_html_reader(url):
+    """This reader uses the requests library to fetch a page and get its markup"""
     return requests.get(url).text
 
 
 def test_html_reader(url):
+    """This reader is use in test to open a local file and read its markup"""
     return codecs.open(url, encoding='utf-8').read()
